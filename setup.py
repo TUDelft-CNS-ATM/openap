9968acc79a1309e67555f4393a577cacd9d3dec6
Junzi Sun <junzisun@gmail.com>
2019-03-05 22:21:59 +0100

    add package dependencies

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[''],
+    install_requires=['numpy', 'scipy', 'scikit-fuzzy', 'pandas', 'pyyaml'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
