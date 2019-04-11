ef88f1312780ba05d927c188df36196f53110cd4
Junzi Sun <junzisun@gmail.com>
2019-04-11 15:15:36 +0200

    cruise thrust unit to N

--- tools/gen_engine_cruise_perf.py
+++ tools/gen_engine_cruise_perf.py
@@ -10,8 +10,10 @@ df1 = df1.dropna(subset=(['engine', 'cruise_thrust']))
 
 # sfc: lb / hr / lbf -> kg / s / kN
 # 1 -> 0.0283267
-
 df1.cruise_sfc = (df1.cruise_sfc.astype(float) * 0.0283267).round(4)
+
+# lbs -> N, 4.44822
+df1.cruise_thrust = (df1.cruise_thrust.astype(float) * 4.44822).astype(int)
 
 df1.loc[df1.engine=='CFM56-5A4', 'cruise_sfc'] = df1.loc[df1.engine=='CFM56-5A3', 'cruise_sfc']
 df1.loc[df1.engine=='CFM56-5A5', 'cruise_sfc'] = df1.loc[df1.engine=='CFM56-5A3', 'cruise_sfc']
