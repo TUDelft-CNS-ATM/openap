b459dbf4b2263ca37a620a3a6a5b5a50a5503fc7
Aleksey Druzhinin <alexey@yourtask.ru>
2020-04-27 07:38:18 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'scikit-fuzzy', 'pandas', 'pyyaml'],
+    install_requires=['numpy', 'scipy', 'scikit-fuzzy', 'pandas', 'pyyaml', 'matplotlib'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
