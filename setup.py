dbc077c6f6bcea27b958dbcda94c38e59b39df91
Junzi Sun <junzisun@gmail.com>
2021-10-08 22:53:02 +0200

    version 1.2 release

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.1",
+    version="1.2",
     description="Open Aircraft Performance Model (OpenAP) in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
