bfa9d08cd36daac27b5e658d304e6581c8b75fcf
Junzi Sun <junzisun@gmail.com>
2022-05-05 23:00:44 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -27,44 +27,32 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="openap",
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version="1.2",
     description="Open Aircraft Performance Model (OpenAP) in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # The project's main homepage.
-    # url='https://github.com/junzis/openap',
-    # Author details
+    url="https://openap.dev",
     author="Junzi Sun",
     author_email="junzisun@gmail.com",
-    # Choose your license
-    license="GNU GPL v3",
+    license="GNU LGPL v3",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU LGPL v3",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
     ],
     # What does your project relate to?
-    keywords="OpenAP, Open Aircraft Performance, Open Data",
+    keywords="OpenAP, Aircraft Performance, Open Data",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
