46d02d37b599003d36514292f23eb33615f775a2
Junzi Sun <junzisun@gmail.com>
2022-09-12 14:46:25 +0200

    fix bug

--- openap/extra/aero.py
+++ openap/extra/aero.py
@@ -374,9 +374,9 @@ def crossover_alt(v_cas, mach):
         float or ndarray: Altitude (m).
 
     """
-    mach = 1e-4 if mach < 1e-4 else mach
+    mach = np.where(mach < 1e-4, 1e-4, mach)
     delta = ((0.2 * (v_cas / a0) ** 2 + 1) ** 3.5 - 1) / (
-        (0.2 * mach ** 2 + 1) ** 3.5 - 1
+        (0.2 * mach**2 + 1) ** 3.5 - 1
     )
     h = T0 / beta * (delta ** (-1 * R * beta / g0) - 1)
     return h
